c5ecf7c262b4a80c5bd2cf27f7131280d18d106f
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-20 22:35:01 -0300

    Aula 20/02/2019 - Segunda Aula

---
+++ 06-WS-SOAP-Provider/src/br/com/fiap/bo/CambioBO.java
@@ -0,0 +1,17 @@
+package br.com.fiap.bo;
+
+public class CambioBO {
+	
+	public double converterRealDolar(double real) {
+		return real*3.73;
+	}
+	
+	public double converterDolarReal(double dolar) {
+		return dolar/0.03;
+	}
+	
+	public double converterBitcoinReal(double bitcoin) {
+		return bitcoin*14600;
+	}
+
+}
